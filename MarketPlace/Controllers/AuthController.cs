319ba91f1808e4bfc0d8818a9961731f23dd8aca
andriy22 <istep.andriy@gmail.com>
2019-08-20 23:29:20 +0300

    added register action

---
+++ MarketPlace/Controllers/AuthController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarketPlace.Entities.DBEntities;
+using MarketPlace.Helpers;
+using MarketPlace.Models;
+using MarketPlace.Models.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly DBContext _context;
+        public AuthController(UserManager<User> userManager, DBContext dBContext)
+        {
+            this._userManager = userManager;
+            this._context = dBContext;
+        }
+        [HttpPost]
+        // ACTION IS ASYNC
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            try
+            {
+                if (ViewModelsValidator.isValid(model))
+                {
+                    foreach (var el in _context.Users)
+                        if (el.NickName.ToLower() == model.NickName.ToLower())
+                            return BadRequest(new { msg = "Nickname is already taken!" });
+                    //if (model.Email.Contains("Admin"))
+                    //    return StatusCode(404, "Oops, Админ хуесос!");
+                    var user = new User() { Email = model.Email,
+                                            NickName = model.NickName,
+                                            UserName = model.Email };
+                    var result = await _userManager.CreateAsync(user, model.Password);
+                    if (result.Succeeded) {
+                        return Ok(new { msg = "Account created!" });
+                    }
+                    else
+                    {
+                        foreach (var el in result.Errors)
+                            return BadRequest(new { msg = el.Description});
+                    }   
+                }
+                return BadRequest(new { msg = "Data is invalid!" });
+            }
+            catch(Exception)
+            {
+                return BadRequest(new { msg = "Server crash!" });
+            }
+        }
+
+
+    }
+}
---
+++ MarketPlace/Helpers/ViewModelsValidator.cs
@@ -0,0 +1,18 @@
+﻿using MarketPlace.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarketPlace.Helpers
+{
+    public static class ViewModelsValidator
+    {
+        static public bool isValid(RegisterViewModel model)
+        {
+            if (!string.IsNullOrEmpty(model.Email) && !string.IsNullOrEmpty(model.NickName) && !string.IsNullOrEmpty(model.Password))
+                return true;
+            return false;
+        }
+    }
+}
