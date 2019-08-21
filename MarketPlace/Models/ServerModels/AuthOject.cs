747cf6d2f2a3468a8c56449c74ae2209330eceb8
andriy22 <istep.andriy@gmail.com>
2019-08-21 18:55:40 +0300

    added authController, register moved to registerController

---
+++ MarketPlace/Models/ServerModels/AuthOject.cs
@@ -0,0 +1,14 @@
+﻿using MarketPlace.Entities.DBEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarketPlace.Models.ServerModels
+{
+    public class AuthOject
+    {
+        public User user { get; set; }
+        public bool isAuth { get; set; }
+    }
+}
