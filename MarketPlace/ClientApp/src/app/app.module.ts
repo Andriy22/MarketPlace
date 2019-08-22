fa9c307cd2a6dd68f395ceaf590eb030ec644ae5
andriy22 <istep.andriy@gmail.com>
2019-08-22 12:22:55 +0300

    updated

--- MarketPlace/ClientApp/src/app/app.module.ts
+++ MarketPlace/ClientApp/src/app/app.module.ts
@@ -1,53 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {RouterModule} from '@angular/router';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import {DefaultModule} from './modules/default/default.module';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { CommonModule } from '@angular/common';
-import { NzSliderModule } from 'ng-zorro-antd/slider';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { NzAvatarModule } from 'ng-zorro-antd/avatar';
-import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { UserLayoutModule } from './modules/user-layout/user-layout.module';
-registerLocaleData(en);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    RouterModule.forRoot([
-      {path: '', loadChildren: './modules/default/default.module#DefaultModule'}
-    ]),
     BrowserModule,
-    NgZorroAntdModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    DefaultModule,
-    UserLayoutModule,
-    NzGridModule,
-    NzDropDownModule,
-    NzBreadCrumbModule,
-    NzAvatarModule,
-    NzMenuModule,
-    NzSliderModule,
-    CommonModule,
-    NzLayoutModule,
-    NzButtonModule
+    AppRoutingModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
